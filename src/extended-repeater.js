5b5c11710f5233fd813ba447b39b37261baa54f5
Elena-Amelia <amelia013@ya.ru>
2022-05-03 02:00:17 +0400

    feat: add solution for carbon-dating and extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, x = options) {
+  if (typeof str !== 'string') { str = String(str); }
+  if (typeof x.addition === 'boolean' || typeof x.addition === 'object' ) { x.addition = String(x.addition); }
+  const options = {
+    repeatTimes: x.repeatTimes || 1,
+    separator: x.separator || '+',
+    addition: x.addition || '',
+    additionSeparator: x.additionSeparator || '|',
+    additionRepeatTimes: x.additionRepeatTimes || 1,
+  }
+  let add = (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes);
+  let add1  = add.slice(0, add.length - options.additionSeparator.length);
+  let result = (str + add1 + options.separator).repeat(options.repeatTimes);
+  let result1 = result.slice(0, result.length - options.separator.length);
+  return result1;
 }
 
 module.exports = {
